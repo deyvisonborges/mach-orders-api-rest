025255d1cf7029c70c76f5c0fec97411291d7e07
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-02-27 16:51:51 -0300

    init project

---
+++ src/main/java/com/mach/machorderrestapi/MachOrderRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.mach.machorderrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachOrderRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachOrderRestApiApplication.class, args);
+	}
+
+}
