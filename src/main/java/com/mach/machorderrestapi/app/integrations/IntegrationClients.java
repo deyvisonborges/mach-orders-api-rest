5518d717d16a38ac5d003c73ae8b33183bd80228
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-03-08 01:21:01 -0300

    add jwt in integration customer and add values secrets

---
+++ src/main/java/com/mach/machorderrestapi/app/integrations/IntegrationClients.java
@@ -0,0 +1,24 @@
+package com.mach.machorderrestapi.app.integrations;
+
+import com.mach.machorderrestapi.shared.exception.IntegrationException;
+import org.springframework.beans.factory.annotation.Value;
+
+public class IntegrationClients {
+	@Value("${integration.url.api.identity}")
+	private String identityURL;
+
+	@Value("${integration.url.api.catalog}")
+	private String catalogURL;
+
+	public String getIdentityURL() {
+		if(this.identityURL == null)
+			throw new IntegrationException("Please, set identity url in application.properties");
+		return this.identityURL;
+	}
+
+	public String getCatalogURL() {
+		if(this.catalogURL == null)
+			throw new IntegrationException("Please, set catalog url in application.properties");
+		return this.catalogURL;
+	}
+}
